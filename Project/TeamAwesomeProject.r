860a00409b38fbc4941c2680c0d512ff8881343e
kbingham-umkc <klb337@umkc.edu>
2014-12-05 14:40:08 -0600

    Initial Subset

--- Project/TeamAwesomeProject.r
+++ Project/TeamAwesomeProject.r
@@ -5,3 +5,15 @@ mlbdata = read.csv("mlbdata.csv", sep=",")
 fix(mlbdata)
 
 
+#Load leaps so we limit the # of dimensions.
+library(leaps)
+
+
+newMLBData = na.omit(mlbdata)
+attach(newMLBData)
+
+regfit.full = regsubsets(newMLBData$YearP1W., newMLBData)
+
+summary(regfit.full)
+
+
